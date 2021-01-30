4a48c55d3ed0c5989a1ac78ca41f33c89b27d6f4
Engin Topuzoglu <engin91@gmx.de>
2021-01-30 12:41:28 +0100

    move box to module

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require("laser")
 require("player")
+require("box")
 
 function love.load()
     images = {
@@ -16,7 +17,7 @@ function love.load()
     h = images.player:getHeight()
     world = love.physics.newWorld(0, 0, true)
     walls = {create_obstacle(0, 0, 100, 100)}
-    blocks = {create_block(600, 500, 100, 100)}
+    boxes = {create_box(600, 500, 100, 100, "assets/gfx/container001.png")}
 
     objects = {} -- table to hold all our physical objects
 
@@ -34,22 +35,6 @@ function create_obstacle(x, y, w, h)
     object.body = love.physics.newBody(world, x, y) -- remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
     object.shape = love.physics.newRectangleShape(w, h) -- make a rectangle with a width of 650 and a height of 50
     object.fixture = love.physics.newFixture(object.body, object.shape) -- attach shape to body
-    return object
-end
-
-function create_block(x, y, w, h)
-    -- let's create the ground
-    object = {}
-    object.x = x
-    object.y = y
-    object.w = w
-    object.h = h
-    object.body = love.physics.newBody(world, x, y, "dynamic") -- remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
-    object.body:setLinearDamping(3) -- place the body in the center of the world and make it dynamic, so it can move around
-    object.shape = love.physics.newRectangleShape(w, h) -- make a rectangle with a width of 650 and a height of 50
-    object.fixture = love.physics.newFixture(object.body, object.shape) -- attach shape to body
-    object.fixture:setRestitution(0.5) -- let the ball bounce
-
     return object
 end
 
@@ -86,11 +71,11 @@ function love.draw()
     for i, v in ipairs(walls) do
         render_local_box(v.body:getX(), v.body:getY(), v.w, v.h)
     end
-    for i, v in ipairs(blocks) do
-        love.graphics.setColor(0.76, 0.18, 0.05) -- set the drawing color to red for the ball
-        render_local_box(v.body:getX(), v.body:getY(), v.w, v.h)
-        love.graphics.setColor(1, 1, 1) -- set the drawing color to red for the ball
+
+    for i, v in ipairs(boxes) do
+        v.draw()
     end
+
     love.graphics.rectangle("fill", 300, 200, 64, 64)
     laser:draw()
 end
