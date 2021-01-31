4f95dc3e79a15aa6d7f61e69e5c057f5fef16587
patrick <patrick@smokeless.world>
2021-01-31 13:40:53 +0100

    multiple enemies

--- main.lua
+++ main.lua
@@ -14,11 +14,10 @@ require("enemy")
 
 require("capacitor")
 
-
 Talkies = require("libs/talkies")
 gameOver = false
 win = false
-volume=0.1
+volume = 0.1
 textblock = 0;
 function love.load()
     -- love.graphics.setBackgroundColor(0.8, .8, .8)
@@ -32,7 +31,7 @@ function love.load()
     }
     images = {
         player = love.graphics.newImage("assets/gfx/player.png"),
-   
+
         win = love.graphics.newImage("assets/gfx/screenWinning001.png"),
         lose = love.graphics.newImage("assets/gfx/screenLosing001.png")
     }
@@ -42,7 +41,7 @@ function love.load()
     -- game
     love.window.setMode(1000, 1000)
 
-    love.audio.setVolume( volume )
+    love.audio.setVolume(volume)
 
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
@@ -91,13 +90,14 @@ function love.load()
 
     containers = {create_container(600, 800, 100, 100, containerTypes.file),
                   create_container(300, 500, 100, 100, containerTypes.image), create_container(1500, 1700, 100, 100),
-                  create_container(1500, 1800, 100, 100), create_container(1500, 900, 100, 100,containerTypes.image)}
+                  create_container(1500, 1800, 100, 100), create_container(1500, 900, 100, 100, containerTypes.image)}
 
     objects = {} -- table to hold all our physical objects
 
     laser = create_laser(1040, -50, 650, world, 1)
     laser2 = create_laser(1040, 1615, 565, world, 2)
-    enemy = create_enemy(1250, 1400, 1800, 1400, world)
+    enemies = {create_enemy(-190, 2200, 130, 2200, world),create_enemy(1250, 1400, 1800, 1400, world), create_enemy(1900, 400, 2600, 400, world),
+               create_enemy(2000, 600, 2000, 1300, world, true),create_enemy(600, 1500, 600, 2100, world, true)}
     laser_activator = create_laser_activator(850, 800, 100, 100, world, 1)
     laser_activator2 = create_laser_activator(850, 1800, 100, 100, world, 2)
     goals = {create_goal(280, 1450, 100, 100, world)}
@@ -108,13 +108,14 @@ function love.load()
     Talkies.say("Old Robotman Jenkins",
         "Hi there, Kid. I'm so sad, I lost my old wedding foto files\nWould you be so kind to push them into the file converter, so we can restore them?\nMy wife will be sooo mad if you don't help me! If there was just a way to get past that laser...\n\n( Press SPACE to close this dialog )")
 
-
 end
 
 function love.update(dt)
     world:update(dt) -- this puts the world into motion
     player:update(dt)
-    enemy:update(dt)
+    for i, v in ipairs(enemies) do
+        v:update(dt)
+    end
     laser:update(dt, containers)
 
     if love.keyboard.isDown("escape") then -- press the right arrow key to push the ball to the right
@@ -146,7 +147,7 @@ end
 
 function love.draw()
     -- render_local(images.world, 0, 0)
-   -- background()
+    -- background()
     laser_activator:draw()
     laser_activator2:draw()
     player:draw()
@@ -166,25 +167,27 @@ function love.draw()
 
     laser:draw()
     laser2:draw()
-    enemy:draw()
+    for i, v in ipairs(enemies) do
+        v.draw()
+    end
 
     if (gameOver) then
         love.graphics.setColor(0, 0, 1) -- set the drawing color to red for the ball
         love.graphics.rectangle("fill", 0, 0, width, width)
         love.graphics.setColor(1, 1, 1) -- set the drawing color to red for the ball
         Talkies.nextOption()
-        love.graphics.draw(images.lose, width/2-images.lose:getWidth()/2,height/2-images.lose:getHeight()/2)
+        love.graphics
+            .draw(images.lose, width / 2 - images.lose:getWidth() / 2, height / 2 - images.lose:getHeight() / 2)
 
     elseif (win) then
-        love.graphics.setColor(158/255, 230/255, 223/255) -- set the drawing color to red for the ball
+        love.graphics.setColor(158 / 255, 230 / 255, 223 / 255) -- set the drawing color to red for the ball
         love.graphics.rectangle("fill", 0, 0, width, width)
         love.graphics.setColor(1, 1, 1) -- set the drawing color to red for the ball
-        --love.graphics.print("WIN", 400, 300)
-        love.graphics.draw(images.win, width/2-images.win:getWidth()/2,height/2-images.win:getHeight()/2)
-    elseif x<950 and y<1100 then
+        -- love.graphics.print("WIN", 400, 300)
+        love.graphics.draw(images.win, width / 2 - images.win:getWidth() / 2, height / 2 - images.win:getHeight() / 2)
+    elseif x < 950 and y < 1100 then
         Talkies.draw()
     end
-
 
     love.graphics.setColor(0, 1, 0)
     love.graphics.print("x: " .. x, 10, 10)
