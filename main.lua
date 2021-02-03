ff293ee2f42ca3e32eaaf4f2f3a9548c89b0f01a
Umut Topuzoğlu <uloco@schiefewelt.de>
2021-02-03 12:02:00 +0100

    Refactor player to modules

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 require("laser")
+require("player")
 
 function love.load()
     images = {
@@ -19,13 +20,7 @@ function love.load()
 
     objects = {} -- table to hold all our physical objects
 
-    -- let's create a player
-    objects.player = {}
-    objects.player.body = love.physics.newBody(world, x, y, "dynamic") -- place the body in the center of the world and make it dynamic, so it can move around
-    objects.player.body:setLinearDamping(10) -- place the body in the center of the world and make it dynamic, so it can move around
-    objects.player.shape = love.physics.newCircleShape(32) -- the ball's shape has a radius of 20
-    objects.player.fixture = love.physics.newFixture(objects.player.body, objects.player.shape, 1.5) -- Attach fixture to body and give it a density of 1.
-    objects.player.fixture:setRestitution(0) -- let the ball bounce
+    player = create_player(x, y, world)
     laser = create_laser(300, 0, 200, world)
 end
 
@@ -50,6 +45,7 @@ function create_block(x, y, w, h)
     object.w = w
     object.h = h
     object.body = love.physics.newBody(world, x, y, "dynamic") -- remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+    object.body:setLinearDamping(3) -- place the body in the center of the world and make it dynamic, so it can move around
     object.shape = love.physics.newRectangleShape(w, h) -- make a rectangle with a width of 650 and a height of 50
     object.fixture = love.physics.newFixture(object.body, object.shape) -- attach shape to body
     object.fixture:setRestitution(0.5) -- let the ball bounce
@@ -60,19 +56,19 @@ end
 function love.update(dt)
     world:update(dt) -- this puts the world into motion
     if love.keyboard.isDown("right") then -- press the right arrow key to push the ball to the right
-        objects.player.body:applyForce(10000, 0)
+        player.body:applyForce(10000, 0)
     elseif love.keyboard.isDown("left") then -- press the left arrow key to push the ball to the left
-        objects.player.body:applyForce(-10000, 0)
+        player.body:applyForce(-10000, 0)
     end
 
     if love.keyboard.isDown("up") then -- press the right arrow key to push the ball to the right
-        objects.player.body:applyForce(0, -10000)
+        player.body:applyForce(0, -10000)
     elseif love.keyboard.isDown("down") then -- press the left arrow key to push the ball to the left
-        objects.player.body:applyForce(0, 10000)
+        player.body:applyForce(0, 10000)
     end
 
-    x = objects.player.body:getX() + 16
-    y = objects.player.body:getY() + 16
+    x = player.body:getX() + 16
+    y = player.body:getY() + 16
     laser:update(dt, blocks)
 end
 
